244ccf41c0b8d80a5b28ecd2166b44e5f659de8e
Ayuik <marcosayelen@gmail.com>
2024-12-09 12:54:07 +0100

    carrito

--- form_cp.js
+++ form_cp.js
@@ -3,11 +3,12 @@ showButton.addEventListener("click", function () {
   const alertDialog = document.querySelector("#form_cp");
   alertDialog.showModal();
 });
+
 let cp_number = document.getElementById('show_form').textContent;
 let new_cp = document.getElementById('codigo_postal').value;
 let update = document.getElementById('actualizar');
 function actualizar_cp() {
-    cp_number.innerHTML = new_cp
+    cp_number.textContent = new_cp;
 }
 update.addEventListener('click', actualizar_cp)
 
